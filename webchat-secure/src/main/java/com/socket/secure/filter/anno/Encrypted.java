961a9df24cb02239652f21ec785e78b05144ad5a
浮生如梦 <1330000110@qq.com>
2022-08-17 18:31:27 +0800

    更新doc

--- webchat-secure/src/main/java/com/socket/secure/filter/anno/Encrypted.java
+++ webchat-secure/src/main/java/com/socket/secure/filter/anno/Encrypted.java
@@ -4,10 +4,10 @@ import java.lang.annotation.*;
 
 /**
  * API interface security transmission mark <br>
- * mark the method of this annotation, the spring will decrypt and verify the request before receiving the request. <br>
- * The filtering operation is implemented by {@linkplain com.socket.secure.filter.SecureRequestFilter SecureRequestFilter}. <br>
- * Dynamic interface decryption operations are not supported. <br>
- * Note: this annotation is not valid on non mapping methods.
+ * Mark this annotation, the spring will decrypt and verify the request before receiving the request. <br>
+ * Note: This annotation will strictly check the mark position.
+ * Currently, it can only be marked on the class of @Controller or @RestController,
+ * can also be marked on @RequestMapping or derived annotated methods.
  */
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
