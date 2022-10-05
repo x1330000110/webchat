eafffc3e51f6edbf380b1bd22867e57db599cf9c
浮生如梦 <1330000110@qq.com>
2022-10-05 09:35:39 +0800

    update doc

--- webchat-secure/src/main/java/com/socket/secure/exception/RepeatedRequestException.java
+++ webchat-secure/src/main/java/com/socket/secure/exception/RepeatedRequestException.java
@@ -1,10 +1,7 @@
 package com.socket.secure.exception;
 
-import com.socket.secure.constant.SecureProperties;
-
 /**
  * Repeated request exception<br>
- * If {@linkplain SecureProperties#isExactRequestTime()} is disable,
  * this exception can also occur with multiple requests from the same user within one second
  *
  * @date 2022/3/28
