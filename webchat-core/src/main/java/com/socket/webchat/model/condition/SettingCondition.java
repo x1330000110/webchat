03555ce61f30b503a006904a57eafc9622b0dd33
浮生如梦 <1330000110@qq.com>
2022-10-16 12:21:15 +0800

    update code

--- webchat-core/src/main/java/com/socket/webchat/model/condition/SettingCondition.java
+++ webchat-core/src/main/java/com/socket/webchat/model/condition/SettingCondition.java
@@ -1,18 +1,14 @@
 package com.socket.webchat.model.condition;
 
+import cn.hutool.core.util.EnumUtil;
 import com.socket.webchat.model.enums.Setting;
 import lombok.Setter;
-
-import java.util.Arrays;
 
 @Setter
 public class SettingCondition {
     private String key;
 
     public Setting getSetting() {
-        return Arrays.stream(Setting.values())
-                .filter(e -> e.name().equalsIgnoreCase(key))
-                .findFirst()
-                .orElseThrow(() -> new IllegalStateException("无效参数：" + key));
+        return EnumUtil.likeValueOf(Setting.class, key);
     }
 }
