0afa8d83f1297683f95c36ae2087945a465e8e0d
浮生如梦 <1330000110@qq.com>
2022-10-31 17:10:28 +0800

    修改上传文件限制

--- webchat-core/src/main/java/com/socket/webchat/model/enums/FileType.java
+++ webchat-core/src/main/java/com/socket/webchat/model/enums/FileType.java
@@ -17,11 +17,11 @@ public enum FileType implements IEnum<String> {
     /**
      * 图片
      */
-    IMAGE(300 * 1024, 6353656),
+    IMAGE(1024 * 1024, 6353656),
     /**
      * 视频/文件
      */
-    BLOB(30 * 1024 * 1024, 6353652);
+    BLOB(100 * 1024 * 1024, 6353652);
 
     private final int code;
     private final int size;
