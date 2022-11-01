f535709af486831ceba5ecbd83463619f3d931a6
浮生如梦 <1330000110@qq.com>
2022-11-01 11:21:25 +0800

    移除文件过期判断

--- webchat-core/src/main/java/com/socket/webchat/service/impl/UploadServiceImpl.java
+++ webchat-core/src/main/java/com/socket/webchat/service/impl/UploadServiceImpl.java
@@ -1,12 +1,10 @@
 package com.socket.webchat.service.impl;
 
-import cn.hutool.core.date.LocalDateTimeUtil;
 import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.socket.webchat.constant.Constants;
 import com.socket.webchat.exception.UploadException;
 import com.socket.webchat.mapper.ChatRecordFileMapper;
 import com.socket.webchat.mapper.ChatRecordMapper;
@@ -14,7 +12,6 @@ import com.socket.webchat.model.ChatRecord;
 import com.socket.webchat.model.ChatRecordFile;
 import com.socket.webchat.model.condition.FileCondition;
 import com.socket.webchat.model.enums.FileType;
-import com.socket.webchat.model.enums.MessageType;
 import com.socket.webchat.request.BaiduSpeechRequest;
 import com.socket.webchat.request.LanzouCloudRequest;
 import com.socket.webchat.service.UploadService;
@@ -26,7 +23,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 
 /**
  * 文件上传服务
@@ -86,12 +82,7 @@ public class UploadServiceImpl extends ServiceImpl<ChatRecordFileMapper, ChatRec
         ChatRecord record = chatRecordMapper.selectOne(wrapper2);
         // 检查来源
         if (Wss.checkMessagePermission(record)) {
-            // 检查文件类型过期时间
-            LocalDateTime create = LocalDateTimeUtil.of(file.getCreateTime());
-            MessageType type = record.getType();
-            if (type != MessageType.BLOB || create.plusDays(Constants.FILE_EXPIRED_DAYS).isAfter(LocalDateTime.now())) {
-                return lanzouRequest.getResourceURL(file.getUrl());
-            }
+            return lanzouRequest.getResourceURL(file.getUrl());
         }
         return null;
     }
