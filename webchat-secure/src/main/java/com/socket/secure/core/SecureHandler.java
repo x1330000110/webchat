c799e3709f964e113c0fe243dc6d0b51c89202f2
浮生如梦 <1330000110@qq.com>
2022-11-01 23:51:09 +0800

    format code

--- webchat-secure/src/main/java/com/socket/secure/core/SecureHandler.java
+++ webchat-secure/src/main/java/com/socket/secure/core/SecureHandler.java
@@ -30,11 +30,11 @@ public class SecureHandler {
 
     @PostMapping("/secure")
     private String syncAeskey(@RequestBody String certificate, HttpServletRequest request) {
-        String signature = request.getHeader("signature")
+        String signature = request.getHeader("signature");
         if (signature != null && signature.length() == 160) {
             return core.syncAeskey(certificate, signature.substring(0, 32), signature.substring(32));
         }
-        throw new InvalidRequestException("Invalid request header signature")
+        throw new InvalidRequestException("Invalid request header signature");
     }
 
     @ExceptionHandler(InvalidRequestException.class)
