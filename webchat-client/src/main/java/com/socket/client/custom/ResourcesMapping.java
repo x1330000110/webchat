f3ff1eb8294455f15cdce1e4dac35392eb563d8f
浮生如梦 <1330000110@qq.com>
2022-11-22 14:37:29 +0800

    优化代码

--- webchat-client/src/main/java/com/socket/client/custom/ResourcesMapping.java
+++ webchat-client/src/main/java/com/socket/client/custom/ResourcesMapping.java
@@ -1,4 +1,4 @@
-package com.socket.webchat.custom;
+package com.socket.client.custom;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
