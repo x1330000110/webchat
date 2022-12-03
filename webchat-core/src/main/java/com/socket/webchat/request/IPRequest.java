0a39551e24409a175b67ca5897286350df49aab9
浮生如梦 <1330000110@qq.com>
2022-12-03 16:58:03 +0800

    更换IP解析API

--- webchat-core/src/main/java/com/socket/webchat/request/IPRequest.java
+++ webchat-core/src/main/java/com/socket/webchat/request/IPRequest.java
@@ -10,31 +10,13 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class IPRequest {
-    private static final String URL = "https://apibug.cn/api/ip/?apiKey=1d381bf0c6d5db52490d77f00b9403bf&ip={}";
+    private static final String URL = "http://ip-api.com/json/{}?lang=zh-CN";
 
     /**
-     * 获取IP地址所在城市
-     */
-    public String getCity(String ip) {
-        String body = HttpRequest.get(StrUtil.format(URL, ip)).execute().body();
-        return JSONUtil.parseObj(body).getStr("data");
-    }
-
-    /**
-     * 获取IP所在省
+     * 获取IP地址所在省
      */
     public String getProvince(String ip) {
-        String city = getCity(ip);
-        if (city == null) {
-            return null;
-        }
-        int index = city.indexOf("省");
-        if (index == -1) {
-            index = city.indexOf("市");
-        }
-        if (index == -1) {
-            throw new IllegalArgumentException("无法解析的地区：" + city);
-        }
-        return city.substring(0, index + 1);
+        String body = HttpRequest.get(StrUtil.format(URL, ip)).execute().body();
+        return JSONUtil.parseObj(body).getStr("regionName");
     }
 }
