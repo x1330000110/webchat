36c1797c985eeb457a3094ed8cbc0e8bb02b7d1e
浮生如梦 <1330000110@qq.com>
2023-02-04 17:03:59 +0800

    update code

--- webchat-secure/src/main/java/com/socket/secure/util/IPHash.java
+++ webchat-secure/src/main/java/com/socket/secure/util/IPHash.java
@@ -5,11 +5,20 @@ import com.socket.secure.constant.SecureConstant;
 
 import javax.servlet.http.HttpSession;
 
+/**
+ * IP哈希校验工具
+ */
 public class IPHash {
+    /**
+     * 对比IP地址
+     */
     public static boolean checkHash(HttpSession session, String ip) {
         return SecureUtil.sha1().digestHex(ip).equals(session.getAttribute(SecureConstant.DIGEST_IP));
     }
 
+    /**
+     * 缓存IP地址
+     */
     public static void cacheIPHash(HttpSession session, String ip) {
         session.setAttribute(SecureConstant.DIGEST_IP, SecureUtil.sha1().digestHex(ip));
     }
