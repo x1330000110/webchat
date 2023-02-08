f439c77d99038590384b12c4225f294eaa77bb99
浮生如梦 <1330000110@qq.com>
2023-02-08 12:02:54 +0800

    update doc

--- webchat-core/src/main/java/com/socket/webchat/mapper/SysUserLogMapper.java
+++ webchat-core/src/main/java/com/socket/webchat/mapper/SysUserLogMapper.java
@@ -9,6 +9,9 @@ import java.util.List;
 
 @Repository
 public interface SysUserLogMapper extends BaseMapper<SysUserLog> {
+    /**
+     * 获取每个用户的最新登录日志
+     */
     @Select("SELECT guid,province,create_time FROM sys_user_log WHERE id IN ( SELECT MAX(id) FROM sys_user_log where province is not null GROUP BY guid ) ORDER BY guid")
     List<SysUserLog> selectLatestLogonLogs();
 }
