e4f29a3585260b05be46bd24da4a0dbea514cda8
浮生如梦 <1330000110@qq.com>
2023-03-01 22:55:24 +0800

    add log

--- webchat-server/src/main/java/com/socket/server/custom/SessionKeySupport.java
+++ webchat-server/src/main/java/com/socket/server/custom/SessionKeySupport.java
@@ -21,6 +21,7 @@ public class SessionKeySupport implements KeyExchangeListener {
     @Override
     public void onKeyExchange(HttpSession session, String key) {
         String token = (String) session.getAttribute(Constants.AUTH_TOKEN);
+        log.info("密钥变动事件：[sessionId: {}, token: {}, key: {}]", session.getId(), token, key);
         if (token != null) {
             tokenUserManager.setEncKey(token, key);
         }
@@ -29,6 +30,7 @@ public class SessionKeySupport implements KeyExchangeListener {
     @Override
     public void onSessionClosed(HttpSession session) {
         String token = (String) session.getAttribute(Constants.AUTH_TOKEN);
+        log.info("销毁目标会话：[sessionId: {}, token: {}]", session.getId(), token);
         tokenUserManager.removeUser(token);
     }
 }
